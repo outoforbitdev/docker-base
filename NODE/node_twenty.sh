4a03df0d18e099d364cdd7e0688e99dd038b3793
Jay Mirecki <jarett.mirecki@gmail.com>
2023-11-28 19:55:09 -0600

    test(node): ensure yarn is installed

--- NODE/node_twenty.sh
+++ NODE/node_twenty.sh
@@ -3,3 +3,4 @@
 cat /etc/*-release | head -n 4 | grep "Ubuntu 22.04.3 LTS"
 node --version | head -n 1 | grep v20.9.0
 npm --version | head -n 1 | grep 10.1.0
+yarn --version | head -n 1 | grep 1.22
